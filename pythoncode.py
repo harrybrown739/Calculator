5431a0e2d827b1c2faa14d2717f8c56b5529300b
harrybrown739 <harry.brown@jhgs.bucks.sch.uk>
2025-10-16 10:36:02 +0100

    Moved code
    Moved code from "python code" file to this file as it needed to be labelled as a python file to work correctly.

--- pythoncode.py
+++ pythoncode.py
@@ -1,1 +1,12 @@
-
+num1 = int(input("enter first number"))
+num2 = int(input("enter second number"))
+option = input("multiply, add, divide or subtract")
+if option == multiply:
+ answer=num1*num2
+elif option == add:
+ answer=num1+num2
+elif option == divide:
+ answer=num1/num2
+elif option == subtract:
+ answer=num1-num2
+print(answer)
